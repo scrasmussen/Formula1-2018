898866a0b391671836c888bf9f591c5b76c2346b
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-03-26 18:44:04 +0000

    lawlz

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 PC=python3
 FILE=analyze.py
+NEWFILE=new_analyze.py
 
 all: run
 
 run:
 	$(PC) $(FILE)
+
+new:
+	$(PC) $(NEWFILE)
--- analyze.py
+++ analyze.py
@@ -65,12 +65,19 @@ qualifying2018 = qualifying[ qualifying["raceId"].isin(races2018) &
 # setup dataframes
 points = pd.DataFrame({
   'driverId':drivers2019['driverId'],
+  'driverId':drivers2019['driverId'],
   'surname':drivers2019['surname'],
   'racePoints':0,
   'racesRaced':0,
   'racesFinished':0,
-  'score':0
+  'score':0,
+  'team_score':0,
+  'cost':0.,
+  'worth_score':0.,
+  'worth_team_score':0.
   })
+
+con = results2018.constructorId.unique()
 
 rl = results2018.raceId.unique()
 raceList = pd.DataFrame({
@@ -91,10 +98,10 @@ for index,row in qualifying2018.iterrows():
   else:
     score = 3
   points.loc[driverId,'score'] += score
+  if (score != -5):
+      points.loc[driverId,'team_score'] += score
 
 # calculate fastest lap
-# driverName = points['driverId'] == row['driverId']
-print(points)
 points.loc[points['surname'] == 'Ricciardo', 'score']  += (4 * 5)
 points.loc[points['surname'] == 'Bottas', 'score']     += (7 * 5)
 points.loc[points['surname'] == 'Verstappen', 'score'] += (2 * 5)
@@ -103,31 +110,33 @@ points.loc[points['surname'] == 'Vettel', 'score']     += (3 * 5)
 points.loc[points['surname'] == 'Hamilton', 'score']   += (3 * 5)
 points.loc[points['surname'] == 'Magnussen', 'score']  += (1 * 5)
 
-print(points.loc[points['surname'] == 'Magnussen', 'score'])
-sys.exit()
 for index,row in results2018.iterrows():
   driverId = points['driverId'] == row['driverId']
   test = row['driverId']
-  # print(driverId,"::",test)
-
-  position = row.position
 
   # calculate starting grid points
   gridPoints = calcGridPoints(row.grid)
   # calculate position change points
   (racePoints, raceFinished) = calcPositionChangePoints(row.grid,row.position)
+  if (racePoints == -15):
+    teamPoints = 0
+  else:
+    teamPoints = racePoints
   # calculate finishing position points
   racePoints += row.points
+  teamPoints += row.points
   # calculate if finished ahead of teammate
 
 
   # print(row)
   score = racePoints + raceFinished + gridPoints
+  team_score = teamPoints + gridPoints
 
   points.loc[driverId,'racePoints'] += racePoints
   points.loc[driverId,'racesRaced'] += 1
   points.loc[driverId,'racesFinished'] += raceFinished
   points.loc[driverId,'score'] += score
+  points.loc[driverId,'team_score'] += team_score
 
   # == TODO ==
   # fastest lap
@@ -141,14 +150,63 @@ for index,row in results2018.iterrows():
   # if (row['fastestLap'].isin(points["driverId"])):
   #   print(row)
 
+
+# adding cost and average points per dollar
+points.loc[points['surname'] == 'Hamilton', 'cost']   += 30.5
+points.loc[points['surname'] == 'Vettel', 'cost']     += 27.5
+points.loc[points['surname'] == 'Verstappen', 'cost'] += 24.5
+points.loc[points['surname'] == 'Leclerc', 'cost']    += 23.5
+points.loc[points['surname'] == 'Bottas', 'cost']     += 20.5
+points.loc[points['surname'] == 'Gasly', 'cost']      += 17.5
+points.loc[points['surname'] == 'Ricciardo', 'cost']  += 12.0
+points.loc[points['surname'] == 'Hülkenberg', 'cost'] += 11.8
+points.loc[points['surname'] == 'Pérez', 'cost']      += 11.0
+points.loc[points['surname'] == 'Magnussen', 'cost']  += 10.0
+points.loc[points['surname'] == 'Räikkönen', 'cost']  += 10.0
+points.loc[points['surname'] == 'Grosjean', 'cost']   += 9.3
+points.loc[points['surname'] == 'Sainz', 'cost']      += 7.7
+points.loc[points['surname'] == 'Stroll', 'cost']     += 7.4
+points.loc[points['surname'] == 'Norris', 'cost']     += 6.9
+points.loc[points['surname'] == 'Giovinazzi', 'cost'] += 6.8
+points.loc[points['surname'] == 'Albon', 'cost']      += 6.0
+points.loc[points['surname'] == 'Russell', 'cost']    += 6.0
+points.loc[points['surname'] == 'Kubica', 'cost']     += 5.5
+points.loc[points['surname'] == 'Kvyat', 'cost']      += 5.5
+
+
+# Hamilton
+# Bottas
+# Verstappen
+# Vettel
+# Leclerc
+# Magnussen
+# Hülkenberg
+# Räikkönen
+# Stroll
+# Kvyat
+# Gasly
+# Pérez
+# Albon
+# Giovinazzi
+# Russell
+# Norris
+# Kubica
+# Grosjean
+# Ricciardo
+# Sainz
+
+
+
+ # points.score.div(points.cost, axis='columns')
+points.worth_score = points.score.div(points.cost)
+points.worth_team_score = points.team_score.div(points.cost)
+
+# z = zip(points.surname, points.worth_score)
+z = zip(points.surname, points.score)
 print(points)
+for i in z:
+  print(i)
 print('Fin')
-
-
-
-# printing
-# print(races2018)
-# print(drivers2019["driverID"])
 
 
 
