7c608b9bd98cb09902da1df5017180c168add67e
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-04-02 14:11:24 +0100

    fixing values and taking names

--- startup_2019.py
+++ startup_2019.py
@@ -9,6 +9,8 @@ import sys
 fantasy2019 = ["Week","Name","Points","isDriver"]
 f = pd.read_csv('data/fantasy2019.csv', names=fantasy2019)
 # f = f.drop("Team/Driver",1)
+# f = f[f.Name != "Mercedes"]
+# print(f)
 mean = f.groupby(['Week','Name']).mean()
 ave_points = mean.sort_values('Points',ascending=False)
 
@@ -35,6 +37,7 @@ print(drivers)
 iterDrivers = list(it.combinations(drivers.Name, 5))
 best_points = 0
 best_team   = ''
+
 for driverList in iterDrivers:
   for team in teams.Name:
     team_cost   = teams.loc[teams['Name']==team].Cost.item()
@@ -45,7 +48,11 @@ for driverList in iterDrivers:
 
     team_points = teams.loc[teams['Name']==team].Points.item()
     driver_points = drivers.loc[drivers['Name'].isin(driverList)].Points.sum()
-    total_points = team_points + driver_points
+    turbo = drivers.loc[drivers.Name.isin(driverList) & (drivers.Cost <= 19)]
+    turbo_points = turbo.Points.max()
+    turbo_driver = turbo[turbo.Points == turbo_points].Name
+    total_points = team_points + driver_points + turbo_points
+
 
     if (total_points == best_points):
       print("REPEAT! ",total_points, " and ", total_cost)
@@ -54,15 +61,20 @@ for driverList in iterDrivers:
       best_cost    = total_cost
       best_team    = team
       best_drivers = driverList
+      best_turbo   = turbo_driver
       best_list    = team + ", " + ", ".join(driverList)
       b = ["{0:0.2f}".format(i) for i in [total_cost, total_points]]
-      print("\ncost: "+str(b[0]) + " points: " + str(b[1]))
+      print("\ncost: "+str(b[0]) + " points: " + str(b[1]) + " turbo: " + best_turbo.item())
+      print(best_list)
+    elif (total_points >= best_points - 2):
+      best_list    = team + ", " + ", ".join(driverList)
+      b = ["{0:0.2f}".format(i) for i in [total_cost, total_points]]
+      print("\ncost: "+str(b[0]) + " points: " + str(b[1]) + " turbo: " + best_turbo.item())
       print(best_list)
 
-
-print("---Finished Analysis---")
-print("Best team ", best_team)
-print("Best drivers", best_drivers)
-print("Best points", best_points)
-print("Total cost = ", str(total_cost))
+print("\n---Finished Analysis---")
+print("Best team: " + best_team)
+print("Best drivers: " + ", ".join(best_drivers))
+print("Best points: "  + "{0:0.02f}".format(best_points))
+print("Total cost: "   + "{0:0.02f}".format(best_cost))
 print("\nFIN")
