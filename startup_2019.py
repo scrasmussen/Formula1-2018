469e3ad0b4d6359549fd1a061a9a8d67359594a8
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-04-14 14:07:37 +0100

    cleaner code

--- startup_2019.py
+++ startup_2019.py
@@ -4,21 +4,22 @@ import itertools as it
 import sys
 import time
 
+def pprint(current_group, total_cost, total_points, turbo):
+  b = ["{0:0.2f}".format(i) for i in [total_cost, total_points]]
+  print("\ncost: "+str(b[0]) + " points: " + str(b[1]) + " turbo: " + turbo)
+  print(current_group)
 
-
-# races
+# import race data
 fantasy2019 = ["Week","Name","Points","isDriver"]
 f = pd.read_csv('data/fantasy2019.csv', names=fantasy2019)
-# f = f.drop("Team/Driver",1)
-# f = f[f.Name != "Mercedes"]
-# print(f)
 mean = f.groupby(['Week','Name']).mean()
 ave_points = mean.sort_values('Points',ascending=False)
 
+# import cost data
 cost_names = ["Week","Name","Cost"]
 c1 = pd.read_csv('data/fantasy2019_cost.csv', names=cost_names)
-week3 = c1[c1.Week == 3]
-cost  = week3[["Name","Cost"]]
+current_week = c1[c1.Week == 3]
+cost  = current_week[["Name","Cost"]]
 cost = pd.merge(cost,ave_points,on='Name',how='outer')
 p = cost.sort_values('Points',ascending=False)
 
@@ -39,14 +40,16 @@ print(drivers)
 iterDrivers = list(it.combinations(drivers.Name, 5))
 best_points = 0
 best_team   = ''
+
+# converted teams to zipped list for quicker access
+team_list = list(zip(teams.Name, teams.Points, teams.Cost))
 name_i=0
 points_i=1
 cost_i=2
-team_list = list(zip(teams.Name, teams.Points, teams.Cost))
 # only look at Mercedes cause they are destroying, speeds up algorithm
 team_mercedes = list((t for t in team_list if t[name_i] == 'Mercedes'))
 
-# sys.exit()
+
 start = time.time()
 for driverList in iterDrivers:
   for team in team_mercedes:
@@ -62,25 +65,19 @@ for driverList in iterDrivers:
     turbo_driver = turbo[turbo.Points == turbo_points].Name
     total_points = team_points + driver_points + turbo_points
 
-    if (total_points == best_points):
-      print("REPEAT! ",total_points, " and ", total_cost)
     if (total_points >= best_points):
       best_points  = total_points
       best_cost    = total_cost
       best_team    = team[name_i]
       best_drivers = driverList
       best_turbo   = turbo_driver
-      best_list    = best_team + ", " + ", ".join(driverList)
-      b = ["{0:0.2f}".format(i) for i in [total_cost, total_points]]
-      print("\ncost: "+str(b[0]) + " points: " + str(b[1]) + " turbo: " + best_turbo.item())
-      print(best_list)
+      current_group    = best_team + ", " + ", ".join(driverList)
+      pprint(current_group, total_cost, total_points, best_turbo.item())
     elif (total_points >= 200): # best_points - 2):
-      choices    = team[name_i] + ", " + ", ".join(driverList)
-      b = ["{0:0.2f}".format(i) for i in [total_cost, total_points]]
-      print("\ncost: "+str(b[0]) + " points: " + str(b[1]) + " turbo: " + best_turbo.item())
-      print(choices)
+      current_group = team[name_i] + ", " + ", ".join(driverList)
+      pprint(current_group, total_cost, total_points, best_turbo.item())
 end = time.time()
-# zip list with only Mercedes taking 46.7 seconds
+# zip list with only Mercedes taking 46.7 seconds, 48.58
 
 print("\n---Finished Analysis---")
 print("Took ",end-start," seconds")
