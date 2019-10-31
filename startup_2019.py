869d639eeecc8af81bbcd10555afb736a0ea5b0f
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-10-31 13:11:25 +0000

    Mexican GP

--- startup_2019.py
+++ startup_2019.py
@@ -4,9 +4,9 @@ import itertools as it
 import sys
 import time
 
-# 103.9, 105, 105.8, 106.2, 107.4, 109.9, 110.9, 111, 110.8
-budget = 111.3  # current budget
-week= 17 # current week
+# 103.9, 105, 105.8, 106.2, 107.4, 109.9, 110.9, 111, 110.8, 111.3
+budget = 111.7  # current budget
+week=18 # current week
 starting_week = 1
 
 
